9fc5ba6f6ac1f2b90c283ad41424eb1f01e2b9c4
Arshdeep Dubey <53818463+Arshdeepdubey@users.noreply.github.com>
2024-10-13 01:55:24 +0530

    feat(ARSH): Refactor Changes

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.security_group.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Hello Terraform"
   }
 }
 
-module "security-group" {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
   name    = "blog_new"
